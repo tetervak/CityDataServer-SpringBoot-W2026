34e5c182642cbe601655643742987e05e0a61771
Alex Tetervak <tetervak@sheridancollege.ca>
2026-03-17 21:51:05 -0400

    removed an unused import

--- src/main/java/ca/tetervak/citydata/controller/CitiesController.java
+++ src/main/java/ca/tetervak/citydata/controller/CitiesController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
