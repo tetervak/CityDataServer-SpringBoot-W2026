2ddc71a567398bdd6b9a164a5e9dba035bde2063
Alexander Tetervak <tetervak@sheridancollege.ca>
2026-03-25 10:02:45 -0400

    edited the filter

--- src/main/java/ca/tetervak/citydata/filter/RequestLogFilter.java
+++ src/main/java/ca/tetervak/citydata/filter/RequestLogFilter.java
@@ -4,7 +4,6 @@ package ca.tetervak.citydata.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
@@ -16,8 +15,9 @@ public class RequestLogFilter implements Filter {
                          ServletResponse servletResponse,
                          FilterChain filterChain) throws IOException, ServletException {
         String requestUri = ((HttpServletRequest)servletRequest).getRequestURI();
+        String method = ((HttpServletRequest)servletRequest).getMethod();
 
-        log.info("received request for {}", requestUri);
+        log.info("received {} request for {}", method, requestUri);
         filterChain.doFilter(servletRequest, servletResponse);
         log.info("response send for requested {}", requestUri);
     }
