8ae40a5581266abfec92743b3316b76b1015abfd
Alex Tetervak <tetervak@sheridancollege.ca>
2026-03-24 21:56:39 -0400

    added validation

--- src/main/java/ca/tetervak/citydata/data/City.java
+++ src/main/java/ca/tetervak/citydata/data/City.java
@@ -1,7 +1,10 @@
 package ca.tetervak.citydata.data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
@@ -11,11 +14,21 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 public class City {
-    @Id
+    @Id @NotBlank
     private String cityId;
+
+    @NotBlank
     private String name;
+
+    @Min(0)
     private int population;
+
+    @JsonProperty("capital")
     private boolean isCapital;
+
+    @Min(0)
     private double area;
+
+    @NotBlank
     private String country;
 }
