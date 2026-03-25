09bcc79d628dfd3bb3cb84c2261f161aaf22fd88
Alex Tetervak <tetervak@sheridancollege.ca>
2026-03-24 21:56:39 -0400

    added crud

--- src/main/java/ca/tetervak/citydata/controller/CitiesController.java
+++ src/main/java/ca/tetervak/citydata/controller/CitiesController.java
@@ -6,13 +6,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.List;
@@ -46,5 +44,42 @@ public class CitiesController {
         return cityRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElseThrow(() -> new NoResourceFoundException(HttpMethod.GET, null, "/api/cities/" + id));
+    }
+
+    @PostMapping(produces = "application/json")
+    @Operation(summary = "Create a new city", description = "Creates a new city entry")
+    public City createCity(@RequestBody @Valid City city) {
+        log.trace("createCity() is called with city={}", city);
+        return cityRepository.save(city);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update an existing city", description = "Updates an existing city entry")
+    public ResponseEntity<City> updateCity(@PathVariable String id, @Valid @RequestBody  City city)
+     throws  NoResourceFoundException {
+        log.trace("updateCity() is called with id={} and city={}", id, city);
+        return cityRepository.findById(id)
+                .map(existingCity -> {
+                    existingCity.setName(city.getName());
+                    existingCity.setPopulation(city.getPopulation());
+                    existingCity.setCapital(city.isCapital());
+                    existingCity.setArea(city.getArea());
+                    existingCity.setCountry(city.getCountry());
+                    City updatedCity = cityRepository.save(existingCity);
+                    return ResponseEntity.ok(updatedCity);
+                })
+                .orElseThrow(() -> new NoResourceFoundException(HttpMethod.GET, null, "/api/cities/" + id));
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a city by ID", description = "Deletes a city with the specified ID")
+    public  ResponseEntity<Void> deleteCity(@PathVariable String id) throws  NoResourceFoundException {
+        log.trace("deleteCity() is called with id={}", id);
+        if (cityRepository.existsById(id)) {
+            cityRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+          throw  new NoResourceFoundException(HttpMethod.GET, null, "/api/cities/" + id);
+        }
     }
 }
--- src/main/java/ca/tetervak/citydata/errors/ApiExceptionHandler.java
+++ src/main/java/ca/tetervak/citydata/errors/ApiExceptionHandler.java
@@ -4,15 +4,35 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import java.util.stream.Collectors;
+
 @Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ApiResponse(content = @Content(mediaType = "application/json"))
+    public ApiError handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error(ex.getMessage(), ex);
+        var bindingResult = ex.getBindingResult();
+        String message = "Validation failed for " + bindingResult.getObjectName() + ". " +
+                bindingResult.getFieldErrors().stream()
+                        .map(fieldError -> "Field " + fieldError.getField() + " " + fieldError.getDefaultMessage())
+                        .collect(Collectors.joining("; "));
+        return new ApiError(
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getClass().getSimpleName(),
+                message
+        );
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
